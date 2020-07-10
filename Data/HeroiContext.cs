1641082263197a02eb5f02059c4fc5f326b469a1
Brenda Profiro <brenda.santos@tripletech.com.br>
2020-07-10 16:39:09 -0300

    first commit

---
+++ Data/HeroiContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using EFCore.WebAPI.Models;
+
+namespace EFCore.WebAPI.Data
+{
+    public class HeroiContext : DbContext
+    {
+        public DbSet<Heroi> Herois { get; set; }
+
+        public DbSet<Batalha> Batalhas { get; set; }
+
+        public DbSet<Arma> Armas { get; set; }
+
+        public DbSet<HeroiBatalha> HeroisBatalha { get; set; }
+
+        public DbSet<IdentidadeSecreta> IdentidadesSecretas { get; set; }
+
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<HeroiBatalha>(entity =>
+            {
+                entity.HasKey(e => new { e.BatalhaId, e.HeroiId });
+            });
+        }
+    }
+}
